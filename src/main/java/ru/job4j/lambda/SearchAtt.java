927a00f4e0ab089fc6432400b578d355d3a59c97
Dmitriy K <dmitriyk80@bk.ru>
2020-07-28 13:35:32 +0400

    2.0. Встроенные функциональные интерфейсы.[249209#250868]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -1,6 +1,7 @@
 package ru.job4j.lambda;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -10,16 +11,6 @@ public class SearchAtt {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
             if (att.getSize() > 100) {
-                rsl.add(att);
-            }
-        }
-        return rsl;
-    }
-
-    private static List<Attachment> filterSize(List<Attachment> elements, Predicate<Attachment> predicateFunction) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : elements) {
-            if (predicateFunction.test(att) == att.getSize() > 100) {
                 rsl.add(att);
             }
         }
@@ -36,14 +27,19 @@ public class SearchAtt {
         return rsl;
     }
 
-    private static List<Attachment> filterName(List<Attachment> elements, Predicate<Attachment> predicateFunction) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : elements) {
-            if (predicateFunction.test(att) == att.getName().contains("bug")) {
-                rsl.add(att);
-            }
-        }
-        return rsl;
+    public static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> predicate) {
+
+        return list;
+    }
+
+    private static List<Attachment> filterSize(List<Attachment> elements, Predicate<Attachment> predicateFunction) {
+        predicateFunction = value -> value.getSize() > 100;
+        return elements;
+    }
+
+    private static List<Attachment> filterName(List<Attachment> elements, Predicate<Attachment> predicateFilter) {
+        predicateFilter = value -> value.getName().contains("bug");
+        return elements;
     }
 
 
