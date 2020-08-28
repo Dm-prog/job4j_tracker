38d62f135e886f2b3b1e82a556453bb13a67d6f6
Dm-prog <prostokota@list.ru>
2020-08-28 22:50:48 +0400

    5. Подсчет функции в диапазоне.[24251#250861]

--- src/main/java/ru/job4j/function/ImplementationOfTheFunctions.java
+++ src/main/java/ru/job4j/function/ImplementationOfTheFunctions.java
@@ -8,8 +8,9 @@ public class ImplementationOfTheFunctions {
 
     public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> list = new ArrayList<>();
-
-
+        for (int i = start; i < end; i++) {
+            list.add(func.apply((double) i));
+        }
         return list;
     }
 }
