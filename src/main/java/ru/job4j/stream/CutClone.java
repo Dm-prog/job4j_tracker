adc3adf394b16bc0f2b8a098566d28afdd7c5520
Dm-prog <prostokota@list.ru>
2020-09-29 14:32:35 +0400

    4. Преобразование List в Map.[110226#250878]

--- src/main/java/ru/job4j/stream/CutClone.java
+++ src/main/java/ru/job4j/stream/CutClone.java
@@ -4,23 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CutClone {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("Ivan");
-        names.add("Stepan");
-        names.add("Petrucho");
-        names.add("Ivan");
-        names.stream()
-                .distinct()
-                .forEach(System.out::println);
+        CutClone cutClone = new CutClone();
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Ivanov", 1));
+        students.add(new Student("Stepanov", 2));
+        students.add(new Student("Petrucho", 3));
+        students.add(new Student("Ivanov", 1));
+        System.out.println(cutClone.sortStudent(students));
+    }
 
+    public Map<String, Student> sortStudent(List<Student> studentList) {
         Map<String, Student> collect = List.of(
-                new Student("Иванов", 1),
-                new Student("Петров", 1),
-                new Student("Сидоров", 1))
-                .stream().collect(Collectors.toMap(e -> e.getSurname(), e -> e));
-
+                studentList.get(0),
+                studentList.get(1),
+                studentList.get(2),
+                studentList.get(3))
+                .stream().distinct().collect(Collectors.toMap(e -> e.getSurname(), e -> e));
+        return collect;
     }
 }
